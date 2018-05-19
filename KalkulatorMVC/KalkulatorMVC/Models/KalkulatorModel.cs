131edcd6ea185e3b2d70509ab236c4bcd9c42a5e
DESKTOP-O95R2TQ\Ivo <ggrriiff@net.hr>
2018-05-19 14:53:02 +0200

    Napravljena klasa modela kalkulatora

---
+++ KalkulatorMVC/KalkulatorMVC/Models/KalkulatorModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/*U modele idu samo klase ili interface-i. Služe za definiranje šifrarnika*/
+
+namespace KalkulatorMVC.Models
+{
+    public enum Operacija
+    {
+        Zbrajanje = '+',
+        Oduzimanje = '-',
+        Mnozenje = '*',
+        Dijeljenje = '/'
+    }
+
+    public class KalkulatorModel
+    {
+        public decimal PrviOperand { get; set; }
+        public decimal DrugiOperand { get; set; }
+        public Operacija Operacija { get; set; }
+        public decimal Rezultat { get; set; }
+    }
+}
